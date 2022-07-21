c9e646584dcb09e2e00e658ec6a9e348211c97e2
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2022-07-21 14:50:04 +0900

    Initial Commit

---
+++ GoogleMaps-Sandbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoogleMaps-Sandbox
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2022/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoogleMaps-Sandbox/GoogleMaps_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMaps_SandboxApp.swift
+//  GoogleMaps-Sandbox
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2022/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMaps_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
